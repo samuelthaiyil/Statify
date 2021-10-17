f6d8515ec7c4b9d3cc5e9edbcb89614b7665f6f8
Sam Thaiyil <77406282+samuelthaiyil@users.noreply.github.com>
2021-10-16 20:23:06 -0400

    upgraded project

---
+++ Statify/Controllers/ManageGamesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StatTracker.Controllers
+{
+    public class ManageGamesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Statify/Models/GameDetail.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StatTracker.Models
+{
+    public class GameDetail
+    {
+        public int GameDetailId { get; set; }
+
+        public Game GameId;
+
+        public Player PlayerId;
+    }
+}
