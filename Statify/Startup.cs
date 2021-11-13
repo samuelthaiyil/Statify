2b08f1b42d065b5dabb782d1ac198f9bf21391f6
Sam Thaiyil <77406282+samuelthaiyil@users.noreply.github.com>
2021-11-13 12:52:52 -0500

    Updated startup.cs with google auth

--- Statify/Startup.cs
+++ Statify/Startup.cs
@@ -35,6 +35,13 @@ namespace Statify
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
+
+            services.AddAuthentication().AddGoogle(o =>
+            {
+                IConfigurationSection googleAuth = Configuration.GetSection("Authentication:Google");
+                o.ClientId = googleAuth["ClientId"];
+                o.ClientSecret = googleAuth["ClientSecret"];
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
